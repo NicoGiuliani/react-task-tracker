a531b40518348e13ae8510275d17028a0f4bb9bb
Nico Julian <NicoGiuliani@users.noreply.github.com>
2022-12-08 17:40:21 -0800

    Added ability to add new task

--- src/App.js
+++ src/App.js
@@ -22,17 +22,24 @@ function App() {
     return data
   }
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1 
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const response = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await response.json()
+    setTasks([...tasks, data])
   }
 
   const deleteTask = async (id) => {
     await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'DELETE'
     })
-    
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
